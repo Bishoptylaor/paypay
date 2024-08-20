a3a30dd8d5b5538771fbfe3dc47a9564806a1c04
wangxinyu <19482929+Bishoptylaor@users.noreply.github.com>
2024-08-20 18:47:10 +0800

    [feat]alipay.payment.phone;pc

---
+++ alipay/payment/pc_page.go
@@ -0,0 +1,72 @@
+package payment
+
+import (
+	"github.com/Bishoptylaor/paypay/alipay"
+	"github.com/Bishoptylaor/paypay/alipay/aliClient"
+	"github.com/Bishoptylaor/paypay/alipay/consts"
+	"github.com/Bishoptylaor/paypay/alipay/flow"
+)
+
+type PCPageCaller struct {
+}
+
+func NewPCPageCaller(a *aliClient.Client) flow.PcPagePay {
+	// do some implantation
+	md := PCPageCaller{}
+	aliClient.SetChecker(md.payloadChecker())(a)
+	aliClient.SetPayloadPreSetter(map[string][]alipay.PayloadPreSetter{
+		"alipay.trade.page.pay": []alipay.PayloadPreSetter{
+			preSetter("product_code", "FAST_INSTANT_TRADE_PAY"),
+		},
+	})(a)
+	return a
+}
+
+func (PCPageCaller) payloadChecker() alipay.PayloadRuler {
+	return func(caller string) []string {
+		switch caller {
+		case "alipay.trade.page.pay":
+			return []string{
+				"out_trade_no != nil",
+				consts.TotalAmountDefaultRule,
+				"subject != nil",
+				`product_code == "FAST_INSTANT_TRADE_PAY"`,
+			}
+		case "alipay.trade.refund":
+			return []string{
+				"refund_amount != nil",
+				consts.TradeNo2in1DefaultRule,
+				"refund_reason != nil",
+			}
+		case "alipay.trade.refund.depositback.completed":
+			return []string{
+				"trade_no != nil",
+				"out_trade_no != nil",
+				"out_request_no != nil",
+				"dback_status != nil",
+				"dback_amount != nil",
+			}
+		case "alipay.trade.close":
+			return []string{
+				consts.TradeNo2in1DefaultRule,
+			}
+		case "alipay.trade.fastpay.refund.query":
+			return []string{
+				consts.TradeNo2in1DefaultRule,
+				"out_request_no != nil",
+			}
+		case "alipay.trade.query":
+			return []string{
+				consts.TradeNo2in1DefaultRule,
+			}
+
+		case "alipay.data.dataservice.bill.downloadurl.query":
+			return []string{
+				`bill_type in ["trade", "signcustomer", "merchant_act", "trade_zft_merchant", "zft_acc", "settlementMerge"]`,
+				"bill_date != nil",
+			}
+		default:
+			return []string{}
+		}
+	}
+}
